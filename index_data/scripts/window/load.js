48f372847b116ba8b2d0720b9d25c1449c89e6ad
DragonHtmIL <meirfons1@gmail.com>
2026-03-09 17:17:56 +0200

    Delete index_data directory

--- index_data/scripts/window/load.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function() {
-  if(localStorage.getItem("lang") === null) {
-    localStorage.setItem("lang", "en");
-    window.location.reload();
-  };
-  loadTexts();
-});
